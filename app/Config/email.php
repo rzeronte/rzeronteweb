8ac8a13d837a3653bedde59ed2d445adafb6e9ff
eduardo <eduardo.rzeronte@gmail.com>
2015-01-20 04:10:08 +0100

    Fix

--- app/Config/email.php
+++ app/Config/email.php
@@ -41,19 +41,19 @@ class EmailConfig {
 
     public $default = array(
         'transport' => 'Mail',
-        'from' => 'rzeronte@gmail.com',
+        'from' => 'eduardo.rzeronte@gmail.com',
         'charset' => 'utf-8',
         //'headerCharset' => 'utf-8',
     );
 
     public $smtp = array(
         'transport' => 'Smtp',
-        'from' => array('eduardo.rzeronte@gmail.com' => 'rZeronte Labs'),
+        'from' => array('yourmail@gmail.com' => 'rZeronte Labs'),
         'host' => 'ssl://smtp.gmail.com',
         'port' => 465,
         'timeout' => 30,
-        'username' => 'eduardo.rzeronte@gmail.com',
-        'password' => 'Frusciante12pass',
+        'username' => 'yourmail@gmail.com',
+        'password' => 'yourmail',
         'client' => null,
         'log' => false,
         //'charset' => 'utf-8',
